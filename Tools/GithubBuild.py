4514c547cbca9d5bb77f7b790d03740ceba8eb7b
Freddie <tsfreddiewang@gmail.com>
2020-06-23 09:04:59 +0100

    自动更新码表

--- Tools/GithubBuild.py
+++ Tools/GithubBuild.py
@@ -3,16 +3,16 @@ import os
 
 from git import Repo
 
-# Commit Changes
-JD6Tools.commit()
+# # Commit Changes
+# JD6Tools.commit()
 
 # GITHUB Repo
 repo_path = os.path.join(os.environ['GITHUB_WORKSPACE'])
 
 print("Setting up Git")
 repo = Repo(repo_path).git
-repo.config('user.name', '小涵')
-repo.config('user.email', 'octocat@github.com')
+# repo.config('user.name', '小涵')
+# repo.config('user.email', 'octocat@github.com')
 
 repo.add('-A')
 repo.commit(m="自动更新码表")

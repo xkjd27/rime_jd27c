f5770da0d8fa2fd4bcfc185027eb57a31297b631
小涵 <octocat@github.com>
2020-07-10 03:03:44 +0100

    dup solve batch 3

--- Lambda/DupCodeSolver.py
+++ Lambda/DupCodeSolver.py
@@ -23,39 +23,43 @@ with open(os.path.join(report_path, "词组重码报告.txt"), mode='r', encodin
             dups.append(data.split('\t'))
             data = infile.readline().strip()
         
-        if (len(dups) == 2):
-            if (len(dups[0][1]) != len(dups[1][1])):
-                dups.sort(key=lambda x: len(x[1]))
-                word = dups[1][1]
-                pinyin = set(JDTools.find_word_pinyin_of_code(word, code)).intersection({' '.join(pinyin) for pinyin in JDTools.get_word(word).pinyins()})
-                space = JDTools.find_space_for_word(word, list(pinyin)[0])
-                min_space = 6
-                for length in space[1]:
-                    if length > 4:
-                        min_space = min(min_space, length)
+        # if (len(dups) == 2):
+        #     if (len(dups[0][1]) != len(dups[1][1])):
+        #         dups.sort(key=lambda x: len(x[1]))
+        #         word = dups[1][1]
+        #         pinyin = set(JDTools.find_word_pinyin_of_code(word, code)).intersection({' '.join(pinyin) for pinyin in JDTools.get_word(word).pinyins()})
+        #         space = JDTools.find_space_for_word(word, list(pinyin)[0])
+        #         min_space = 6
+        #         for length in space[1]:
+        #             if length > 4:
+        #                 min_space = min(min_space, length)
 
-                changes.append((word, pinyin, min_space))
-            else:
-                print(code)
-                i = 1
-                for word in dups:
-                    print("%d. %s" % (i, word[1]))
-                    i += 1
+        #         changes.append((word, pinyin, min_space))
+            # else:
+            #     print(code)
+            #     i = 1
+            #     for word in dups:
+            #         print("%d. %s" % (i, word[1]))
+            #         i += 1
 
-                sel = input("choose: ")
-                if (sel.isdigit() and int(sel) <= len(dups) and int(sel) > 0):
-                    sel = int(sel) - 1
-                    word = dups[sel][1]
-                    pinyin = set(JDTools.find_word_pinyin_of_code(word, code)).intersection({' '.join(pinyin) for pinyin in JDTools.get_word(word).pinyins()})
-                    space = JDTools.find_space_for_word(word, list(pinyin)[0])
-                    min_space = 6
-                    for length in space[1]:
-                        if length > 4:
-                            min_space = min(min_space, length)
+            #     sel = input("choose: ")
+            #     if (sel.isdigit() and int(sel) <= len(dups) and int(sel) > 0):
+            #         sel = int(sel) - 1
+            #         word = dups[sel][1]
+            #         pinyin = set(JDTools.find_word_pinyin_of_code(word, code)).intersection({' '.join(pinyin) for pinyin in JDTools.get_word(word).pinyins()})
+            #         space = JDTools.find_space_for_word(word, list(pinyin)[0])
+            #         min_space = 6
+            #         for length in space[1]:
+            #             if length > 4:
+            #                 min_space = min(min_space, length)
 
-                    changes.append((word, pinyin, min_space))
-                else:
-                    break
+            #         changes.append((word, pinyin, min_space))
+            #     else:
+            #         break
+        for dup in dups:
+            word = dup[1]
+            pinyin = set(JDTools.find_word_pinyin_of_code(word, code)).intersection({' '.join(pinyin) for pinyin in JDTools.get_word(word).pinyins()})
+            changes.append((word, pinyin, 6))
 
     for change in changes:
         JDTools.change_word_shortcode_len(*change)

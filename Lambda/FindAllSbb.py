cb2dda0feb4c9f9a3c1ea20b721b4c3b7fcd4461
TsFreddie <tsfreddiewang@gmail.com>
2020-07-10 23:22:14 +0100

    sbb

--- Lambda/FindAllSbb.py
+++ Lambda/FindAllSbb.py
@@ -11,17 +11,17 @@ sbb = {}
 for ci in cis:
     if len(ci.word()) == 2:
         s = JDTools.pinyin2s(list(ci.pinyins())[0][0])
-        b = JDTools.get_char(ci.word()[1]).shape()[:2].replace('e', 'u')
-        code = JDTools.unmarkfly(s[0])+b
+        b = JDTools.get_char(ci.word()[1]).shape()[:2]
+        code = s[0]+b
         if code in sbb:
             sbb[code].append(ci.word())
         else:
             sbb[code] = [ci.word()]
 
-for code in sbb:
+for code, value in sorted(sbb.items()):
     print(code)
     
-    for i in range(0, len(sbb[code]), 10):
-        print("\t" + ",".join(sbb[code][i:i+10]))
+    for i in range(0, len(value), 10):
+        print("\t" + ",".join(value[i:i+10]))
 
     print()
